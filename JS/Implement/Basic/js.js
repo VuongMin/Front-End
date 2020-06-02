c44a44fbef31e8820cfe3f150c87f4b8b35ad67c
victim <vuongm910@gmail.com>
2020-06-02 23:40:57 +0700

    06/02

--- JS/Implement/Basic/js.js
+++
@@ -1,9 +0,0 @@
-//document ready
-document.addEventListener('DOMContentLoaded',function(){
-    var btn =document.querySelector("button");
- //addEventClick
- btn.addEventListener('click',function(){
-  var Ecard= document.getElementsByClassName("row");
-     Ecard[0].classList.toggle("change");
- });
-},false)
